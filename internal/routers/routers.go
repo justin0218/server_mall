f97efa449456fbfbe4de8b608f382b5a36ab6922
justin <justin.hu@tal.com>
2020-10-09 12:13:10 +0800

    token

--- internal/routers/routers.go
+++ internal/routers/routers.go
@@ -24,7 +24,6 @@ func Init() *gin.Engine {
 	}))
 	global := r.Group("mall")
 	global.GET("health", func(context *gin.Context) {
-		panic(1)
 		context.JSON(200, map[string]string{"msg": "ok"})
 		return
 	})
